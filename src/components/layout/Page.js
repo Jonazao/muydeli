843eb65f6bf7d13b79c1c160f637845190ea1ead
Jhonnatan Guerrero <Jhonnatan.Guerrero@foundersworkshop.com>
2022-10-29 00:11:24 -0700

    feat(diners-routes): refactor diners cards for better ux

--- src/components/layout/Page.js
+++ src/components/layout/Page.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 
-export default function Page({ children }) {
+export default function Page({ children, title }) {
   return (
     <Box
       sx={{
-        paddingTop: '40px',
-        paddingBottom: '30px',
+        paddingTop: '24px',
+        paddingBottom: '80px',
       }}
       component="main"
     >
       <Container maxWidth="xl" component="section">
-        {children}
+        <Grid container flexDirection="column" alignItems="center">
+          {title && (
+            <Grid item>
+              <Typography component="div" variant="h4">
+                {title}
+              </Typography>
+            </Grid>
+          )}
+          <Grid item>{children}</Grid>
+        </Grid>
       </Container>
     </Box>
   );
