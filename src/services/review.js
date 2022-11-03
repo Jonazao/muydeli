2c76ac8c1857aecfe93a2af77a9e2f831fba8f25
Dante Gonzalez <danteg@advancio.com>
2022-11-03 16:25:15 -0700

    feat(reviewApi): Change the resource name

--- src/services/review.js
+++ src/services/review.js
@@ -1,30 +1,29 @@
-import { createApi } from "@reduxjs/toolkit/dist/query/react";
-import { axiosBaseQuery } from "../apis/gateway";
-import { getTagLists } from "../helpers/service-tag-list.helper";
+import { createApi } from '@reduxjs/toolkit/dist/query/react';
+import { axiosBaseQuery } from '../apis/gateway';
+import { getTagLists } from '../helpers/service-tag-list.helper';
 
-const resourceName = 'home';
-const reviewTags= getTagLists(resourceName);
+const resourceName = 'reviews';
+const reviewTags = getTagLists(resourceName);
 
-export const reviewAPI=createApi({
-    reducerPath:'reviewApi',
-    baseQuery:axiosBaseQuery(),
-    tagTypes:[resourceName],
-    endpoints:(build)=>({
-        getReview:build.query({
-            query:()=>({url:resourceName,method:'GET'}),
-            providesTags: (result, error, id) => reviewTags.getOneTagList(),
-        }),
-        getReviews:build.query({
-            query:()=>({url:resourceName,method:'GET'}),
-            providesTags: (response) => {
-                const { result } = response;
-                return result ? reviewTags.getPaginatedListTagList(result.map(({ id }) => id)) : reviewTags.getListTagList();
-              },
-        })
-    })
-})
+export const reviewApi = createApi({
+  reducerPath: `${resourceName}Api`,
+  baseQuery: axiosBaseQuery(),
+  tagTypes: [resourceName],
+  endpoints: (build) => ({
+    getReview: build.query({
+      query: () => ({ url: resourceName, method: 'GET' }),
+      providesTags: (result, error, id) => reviewTags.getOneTagList(),
+    }),
+    getReviews: build.query({
+      query: () => ({ url: resourceName, method: 'GET' }),
+      providesTags: (response) => {
+        const { result } = response;
+        return result
+          ? reviewTags.getPaginatedListTagList(result.map(({ id }) => id))
+          : reviewTags.getListTagList();
+      },
+    }),
+  }),
+});
 
-export const{
-    useGetReviewQuery,
-    useGetReviewsQuery
-} = reviewAPI;
+export const { useGetReviewQuery, useGetReviewsQuery } = reviewApi;
