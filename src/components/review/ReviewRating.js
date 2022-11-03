0cf443ba33d527bf49f9667e0aa99f15db902e53
Dante Gonzalez <danteg@advancio.com>
2022-11-03 16:30:35 -0700

    feat(ReviewRating):Change name of Rating component

--- src/components/review/ReviewRating.js
+++ src/components/review/ReviewRating.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Rating, Typography } from '@mui/material';
-export default function ReviewRating({text,variant,score}) {
-  return (   
+export default function ReviewRating({ text, variant, score }) {
+  return (
     <>
-        <Typography variant={variant} paragraph>
-            {text}
-        </Typography>
-        <Rating name="half-rating" defaultValue={score} precision={0.5} readOnly/>
+      <Typography
+        variant={variant}
+        paragraph
+      >
+        {text}
+      </Typography>
+      <Rating
+        name="review-rating"
+        defaultValue={score}
+        precision={0.5}
+        readOnly
+      />
     </>
   );
 }
