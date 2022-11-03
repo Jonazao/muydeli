f812f685a06b5a949b6e936bcd8dcb428c6b54ab
Dante Gonzalez <danteg@advancio.com>
2022-11-02 17:06:51 -0700

    Remove unused imports

--- src/components/review/ReviewInfo.js
+++ src/components/review/ReviewInfo.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import Typography from '@mui/material/Typography';
-import { CardContent, Rating } from '@mui/material';
+import { CardContent } from '@mui/material';
 import ReviewRating from './ReviewRating';
-export default function ReviewInfo({dish, restaurantName, finalScore}) {
+export default function ReviewInfo({ dish, restaurantName, finalScore }) {
   return (
     <CardContent>
-     <Typography variant="h5">
-       {dish.name}
-     </Typography>
-     <Typography variant="subtitle1" color="text.secondary">
-       {dish.type}  
-     </Typography>
-     <Typography variant="subtitle1" color="text.secondary">
+      <Typography variant='h5'>{dish.name}</Typography>
+      <Typography
+        variant='subtitle1'
+        color='text.secondary'
+      >
+        {dish.type}
+      </Typography>
+      <Typography
+        variant='subtitle1'
+        color='text.secondary'
+      >
         {restaurantName}
-     </Typography>
-     <ReviewRating score={finalScore} text={'Total Score'} variant={'h6'}/>
+      </Typography>
+      <ReviewRating
+        score={finalScore}
+        text={'Total Score'}
+        variant={'h6'}
+      />
     </CardContent>
   );
 }
