9f93a167b038d5241ea1191c7d038b6a88074757
Dante Gonzalez <danteg@advancio.com>
2022-11-03 16:33:48 -0700

    feat(ReviewInfo):Change into double quotes

--- src/components/review/ReviewInfo.js
+++ src/components/review/ReviewInfo.js
@@ -2,26 +2,29 @@ import React from 'react';
 import Typography from '@mui/material/Typography';
 import { CardContent } from '@mui/material';
 import ReviewRating from './ReviewRating';
+import labelsCriteria from '../../constants/criteria/labels';
 export default function ReviewInfo({ dish, restaurantName, finalScore }) {
+  const { finalScoreLabel } = labelsCriteria;
+  const { name, type } = dish;
   return (
     <CardContent>
-      <Typography variant='h5'>{dish.name}</Typography>
+      <Typography variant="h5">{name}</Typography>
       <Typography
-        variant='subtitle1'
-        color='text.secondary'
+        variant="subtitle1"
+        color="text.secondary"
       >
-        {dish.type}
+        {type}
       </Typography>
       <Typography
-        variant='subtitle1'
-        color='text.secondary'
+        variant="subtitle1"
+        color="text.secondary"
       >
         {restaurantName}
       </Typography>
       <ReviewRating
         score={finalScore}
-        text={'Total Score'}
-        variant={'h6'}
+        text={finalScoreLabel}
+        variant="h6"
       />
     </CardContent>
   );
