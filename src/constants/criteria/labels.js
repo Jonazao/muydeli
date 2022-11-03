ea2ddd8d1d68bd75a4c13eb9e08ed69a0e5cb2d4
Dante Gonzalez <danteg@advancio.com>
2022-11-03 16:36:11 -0700

    feat(labelsCriteria): Create objet of labels

---
+++ src/constants/criteria/labels.js
@@ -0,0 +1,10 @@
+const labelsCriteria = {
+  finalScoreLabel: 'Total score',
+  firstImpressionLabel: 'First Impression',
+  platingLabel: 'Plating',
+  satietyLevelLabel: 'Satiety Level',
+  garnishesLabel: 'Garnishes',
+  expectationsLabel: 'Expectations',
+  flavorLabel: 'Flavor',
+};
+export default labelsCriteria;
