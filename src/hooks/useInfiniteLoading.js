4ada607098d6fcda2b7cf7fae183e786a2770520
Jhonnatan Guerrero <Jhonnatan.Guerrero@foundersworkshop.com>
2022-11-07 22:47:01 -0800

    feat(infiniteScroll): remove query params from hook

--- src/hooks/useInfiniteLoading.js
+++ src/hooks/useInfiniteLoading.js
@@ -1,34 +1,26 @@
-import { useState, useEffect, useCallback, useMemo, useRef } from 'react';
-import { useSearchParams } from 'react-router-dom';
+import { useState, useEffect, useCallback, useRef } from 'react';
 import { useInView } from 'react-cool-inview';
 import { isNil } from '../validations/is-nil';
-export default function useInfiniteLoading({ fetchItems, useQueryParamsPagination = false }) {
-  const [searchParams, setSearchParams] = useSearchParams();
 
+const mergeMethods = {
+  append: 'append',
+  prepend: 'prepend',
+};
+
+export default function useInfiniteLoading({ fetchItems }) {
   // Memorize loaded pages
-  const initialPageNumber = useRef(useQueryParamsPagination ? searchParams.get('pageNumber') || 1 : 1);
+  const initialPageNumber = useRef(1);
   const lowestPageNumber = useRef(initialPageNumber.current);
   const highestPageNumber = useRef(initialPageNumber.current);
 
-  const pageSize = useRef(searchParams.get('pageSize') || 15);
+  const pageSize = useRef(15);
   const initialPageLoaded = useRef(false);
   const [hasNext, setHasNext] = useState(true);
   const [hasPrevious, setHasPrevious] = useState(true);
   const [items, setItems] = useState([]);
 
-  const paramObj = useMemo(() => {}, []);
-  for (let p of searchParams) {
-    paramObj[p[0]] = p[1];
-  }
-
   const loadItems = useCallback(
     async (page, limit, mergeMethod) => {
-      useQueryParamsPagination &&
-        setSearchParams({
-          ...paramObj,
-          pageNumber: Number(page),
-          pageSize: Number(limit),
-        });
       const { data } = await fetchItems({
         pageNumber: page,
         pageSize: limit,
@@ -36,9 +28,11 @@ export default function useInfiniteLoading({ fetchItems, useQueryParamsPaginatio
       const { result, pagination } = data;
       setHasNext(!isNil(pagination.urls.next));
       setHasPrevious(!isNil(pagination.urls.previous));
-      setItems((prevItems) => (mergeMethod === 'append' ? [...prevItems, ...result] : [...result, ...prevItems]));
+      setItems((prevItems) =>
+        mergeMethod === mergeMethods.append ? [...prevItems, ...result] : [...result, ...prevItems],
+      );
     },
-    [fetchItems, paramObj, setSearchParams, useQueryParamsPagination],
+    [fetchItems],
   );
 
   useEffect(() => {
@@ -46,7 +40,7 @@ export default function useInfiniteLoading({ fetchItems, useQueryParamsPaginatio
       return;
     }
 
-    loadItems(initialPageNumber.current, pageSize.current, 'append');
+    loadItems(initialPageNumber.current, pageSize.current, mergeMethods.append);
     initialPageLoaded.current = true;
   }, [loadItems]);
 
@@ -54,36 +48,21 @@ export default function useInfiniteLoading({ fetchItems, useQueryParamsPaginatio
     // No more pages to load just for safety
     if (!hasNext) return;
     const nextPage = Number(highestPageNumber.current) + 1;
-    useQueryParamsPagination &&
-      setSearchParams({
-        ...paramObj,
-        pageNumber: Number(nextPage),
-        pageSize: Number(pageSize.current),
-      });
-    loadItems(nextPage, pageSize.current, 'append');
-    console.log(nextPage);
+    loadItems(nextPage, pageSize.current, mergeMethods.append);
     highestPageNumber.current = nextPage;
-    console.log(highestPageNumber.current);
   };
 
   const loadPrevious = () => {
     // No more pages to load just for safety
     if (!hasPrevious) return;
     const previousPage = Number(lowestPageNumber.current) - 1;
-    useQueryParamsPagination &&
-      setSearchParams({
-        ...paramObj,
-        pageNumber: Number(previousPage),
-        pageSize: Number(pageSize.current),
-      });
-    loadItems(previousPage, pageSize.current, 'prepend');
+    loadItems(previousPage, pageSize.current, mergeMethods.prepend);
     lowestPageNumber.current = previousPage;
   };
 
   const { observe: loadNextObserve } = useInView({
     rootMargin: '300px 0px',
     onEnter: () => {
-      console.log('observer!');
       loadNext();
     },
   });
